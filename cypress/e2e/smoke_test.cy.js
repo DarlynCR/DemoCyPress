a3f89d8b40a508b48b1e3b1e06c60e06c719c534
DarlynCR <dacaicedo93@misena.edu.co>
2023-08-28 18:11:17 -0500

    Run test headless

--- cypress/e2e/smoke_test.cy.js
+++ cypress/e2e/smoke_test.cy.js
@@ -1,11 +1,16 @@
 /// <reference types="Cypress" />
 
 
-describe('Test Suite Zerro Bakn, () => {
+describe('Test Suite Zerro" Bakn', () => {
+
+
+    beforeEach(() => {
+        cy.visit('http://zero.webappsecurity.com')
+        
+    });
 
     it('TC1 - Test Home Image', () => {
 
-        cy.visit('http://zero.webappsecurity.com')
         cy.get('.active > img').should('be.visible')
         cy.get('.active > .custom > h4').contains('Online Banking')
       
@@ -13,7 +18,6 @@ describe('Test Suite Zerro Bakn, () => {
 
     it('TC2 - Test E2E - Transfer of founds ', () => {
         
-        cy.visit('http://zero.webappsecurity.com')
         cy.get('#signin_button').click()
         cy.get('#user_login').type("username")
         cy.get('#user_password').type("password")
@@ -29,15 +33,19 @@ describe('Test Suite Zerro Bakn, () => {
 
     })
 
-    it.only('TC3 - Test E2E - Validate interacción with money map', () => {
+    it('TC3 - Test E2E - Validate interacción with money map', () => {
         
-        cy.visit('http://zero.webappsecurity.com');
         cy.get('#signin_button').click()
         cy.get('#user_login').type("username")
         cy.get('#user_password').type("password")
         cy.get('.btn').click()
         cy.get('#money_map_tab > a').click()
         cy.get('#ext-sprite-1275').should("be.visible")
-
+        
     });
+
+    
+
+
+
 })
